d2094a6208066e828c0f491da71efcc69b90eeb2
influxiq1 <himadribar.influxiq@gmail.com>
2020-09-16 23:20:39 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { Test1Component } from './test1/test1.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 't1', component: Test1Component }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
+
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { CommonModule } from '@angular/common';
+import { Test1Component } from './test1/test1.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyCvzWjkeGYWlGTo-EUbqXFElWZ0uEw5yfc',
+      // libraries: ['places']
+    }),
+    CommonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppModule { }
