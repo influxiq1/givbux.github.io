e268cab78569bdc1f483687d04d87768dcf28bc4
influxiq1 <himadribar.influxiq@gmail.com>
2020-10-04 08:55:19 +0530

    up-to-date

--- src/app/tracklist/tracklist.component.ts
+++ src/app/tracklist/tracklist.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
 import { ApiService } from '../api.service';
-
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-tracklist',
   templateUrl: './tracklist.component.html',
@@ -10,169 +9,216 @@ import { ApiService } from '../api.service';
 })
 export class TracklistComponent implements OnInit {
 
-  public user_type: any;
-  public contactuslistFlag: boolean = false;
-  // componentRef: any;
-  // datasource: any;
 
+  componentRef: any;
+  datasource: any;
+  status_gretterthan_zero: any;
+  pendingapplication_view: any;
+  joquuserlist: any;
+  custombutton: any = { label: 'my tree', fields: ['type', 'name', '_id'], url: 'http://localhost:4200/affiliate-tree' };
+  placeholder: any = ['placeholder'];
+  type: any = ['text'];
+  name: any = ['Username'];
+
+  public minDate = new Date(2020, 8, 24);
+  public maxDate = new Date(2020, 8, 31);
 
   // use for Download the PDF
 
-  custom_link: any = '';
+  custom_link: any = [];
+
 
 
   // use for grab the link
-  grab_link: any = [];
+  grab_link: any = {
+      colom_name: [
+          {
+              col_name: 'author',
+              field_name: 'author'
+          }],
+      field: [
+          {
+              label: 'shatterblok grab url',
+              url: 'artixtxp.com/',
+              action: 'null'
+          }]
 
-  myTable: any = [];
+  };
+
+  pendingmodelapplicationarray: any = [];
+
+  public status: any = [{ val: 1, 'name': 'Active' }, { val: 0, 'name': 'Inactive' }];
+
+  // use for status search
+
+  statusarray: any = [{ val: 1, name: 'Approve' }, { val: 4, name: 'Decline' }, { val: 3, name: 'Lock' }];
+
+  editroute: any = 'editroute';
 
 
+  // use for Table Header modification 
 
-
-
-  public status: any = [{ val: 0, name: 'Active' }, { val: 1, name: 'Inactive' }, { val: 1, name: 'Lock' }];
-
-
-  statusarray: any = [{ val: 0, name: 'Active' }, { val: 1, name: 'Inactive' }, { val: 3, name: 'Lock' }];
-
-
-  // emailarray: any = [{ val: 'sourotest222@gmail.com', name: 'sourotest222@gmail.com' }, { val: 'octtest@yopmail.com', name: 'octtest@yopmail.com' }, { val: 'septest@yopmail.com', name: 'septest@yopmail.com' }];
-
-
-  editroute: any = 'param';
-
-
-
+  // Like Table head name is " firstname" => "First Name"
   modify_header_array: any = {
-    address: 'Address',
+      'firstname': "First Name",
+      'email': 'Email Id',
+      
   };
 
 
-
-  tabledata_header_skip: any = ['ip'];
-
-
-
-
-  tabledata_detail_skip: any = ['ip', '_id', 'email_search', 'name_search'];
+  // use for Table Header Skip 
+  pendingmodelapplicationarray_skip: any = ['_id', 'video_thamnail', 'type', 'password', 'blogs_image', 'created_at'];
 
 
 
-  tabledata_detail_datatype: any = [{
-    key: 'images',
-    value: 'image',
-    fileurl: 'http://18.222.26.198/upload/brandimages/'
-  }];
+  // use for Table Detail Field Skip
+  pendingmodelapplicationarray_detail_skip: any = ['_id', 'email', 'name', 'blogtitle', 'Blogs image'];
 
 
+  // use for Table Detail inside the modal image path
+  pendingmodelapplicationarray_detail_datatype: any = [];
+
+  // updateendpoint is use for data update endpoint
   updateendpoint = 'addorupdatedata';
 
-  deleteendpoint = 'api/deletecontactusdata';
+  // deleteendpoint is use for data delete endpoint
+  deleteendpoint = 'deletesingledata';
 
-
+  // this is a database collection name
   tablename = 'users';
 
-
+  // searchendpoint is use for data search endpoint
   searchendpoint = 'datalist';
 
+  // use for click to another page routing path
   click_to_add_ananother_page = '/adminlist';
 
 
 
-
+  // date_search_endpoint is use for date search endpoint
   date_search_endpoint: any = 'datalist';
-
+  // send basic limit data
   limitcond: any = {
-    limit: 10,
-    skip: 0,
-    pagecount: 1
+      "limit": 10,
+      "skip": 0,
+      // "AUth": 1
   };
 
+  // other data
+  libdata: any = {
 
- public libdata: any = {
-
-    detailview_override: [
-      { key: 'addedtime', val: 'Contacted On' },
-    ],
-
-    updateendpoint: 'statusupdate',
-    hideeditbutton: true,
-    hidedeletebutton: false,
-    hideviewbutton: false,
-    hidestatustogglebutton: true,
-    hideaction: false,
-    tableheaders: ['trackablename', 'address', 'addedtime'],
-
-
-  };
-
+      updateendpoint: 'statusupdate',
+      updateendpointmany: 'updateendpointmany',
+      deleteendpointmany: 'deleteendpointmany',
+      // hideeditbutton: true,// all these button options are optional not mandatory
+      // hidedeletebutton: true,
+      //hideviewbutton:false,
+      hidestatustogglebutton: true,
+      hidemultipleselectbutton: null,
+      hidecounter: null,
+      // hidedeletemany: true,
+      // hideupdatemany: false,
+      hideaction: null,
+      // actioncolname: 'Actn',
+      tableheaders: ['authorn', 'priority', 'blogtitle'], //not required
+ 
+  }
+  // send basic sort data
   sortdata: any = {
-    type: 'desc',
-    field: '_id',
-    options: ['addedtime']
+      "type": 'desc',
+      "field": 'priority',
+      "options": ['priority', 'author', 'category', 'blogtitle']
   };
 
 
-
+  // this is a database collection or view name
   date_search_source: any = 'admin_blog_list';
-
-  datacollection: any = 'getcontactusdata';
-
+  // datacollection
+  datacollection: any = 'getadminbloglistdata';
+  //source count
   date_search_source_count: any = 0;
-
   search_settings: any = {
 
-    // datesearch: [{ startdatelabel: 'Contacted On Start Date', enddatelabel: 'Conatcted On End Date', submit: 'Search', field: 'created_datetime' }],   
+      // datesearch: [{ startdatelabel: "Start Date", enddatelabel: "End Date", submit: "Search", field: "created_datetime" }],   // this is use for  date search
 
-    //selectsearch:[{ label: 'Search By Status', field: 'status', values: this.status }], 
+      // selectsearch: [
+      //     { label: 'Search By Status', field: 'status', values: this.status }
+      // ], // this is use for  select search
 
-    textsearch: [{ label: "Search By address", field: 'address' },
-    { label: "Search by trackablename", field: 'trackablename' }],
-
-
+      textsearch: [{ label: "Search By Title", field: 'blogtitle_search' }]
   };
 
-  constructor(public router: Router, private route: ActivatedRoute, public _apiService: ApiService, private cookieService: CookieService) {
-
-    // this.user_type = JSON.parse(this.cookieService.get('type'));
+  // this is search block 
 
 
-    // this.datasource = '';
-    const endpoint = 'addalldata';
-    const endpointc = 'addalldata-count';
 
-    const data: any = {
-      condition: {
-        limit: 10,
-        skip: 0
-      },
-      sort: {
-        type: 'desc',
-        field: '_id',
+  brandarray: any = [];
+  notpendingapplication_view: any = [];
+  adminlist: any = [];
+
+  editroute1: any = 'modeledit';
+
+  temtdata: any = '';
+  // formdata
+  formfieldrefresh: boolean = false;
+  updatetable: boolean = false;
+  formfieldrefreshdata: any = null;
+
+  subscriptions: Subscription[] = [];
+  subscriptioncount: number = 0;
+
+  constructor(public router: Router, private route: ActivatedRoute, public _apiService: ApiService) {
+      // console.log(this.blog_cat_list);
+      // console.log(this.authval)
+      // console.log('custom_link');
+      // console.log(this.custom_link);
+      // console.log(this.formdata, 'formdataformdataformdataformdataformdata')
+      this.datasource = '';
+      let endpoint = 'addalldata'; // for main data endpoint
+      let endpointc = 'addalldata-count'; // for count endpoint
+      // let autodataendpoint = 'exitsing-list'; // for count endpoint
+      // data param for conditionlimit and search
+      let data: any = {
+          "condition": {
+              "limit": 10,
+              "skip": 0
+          },
+          sort: {
+              "type": 'desc',
+              "field": 'priority'
+          },
+          // searchcondition: { status: { $lte: 4 } }
+          searchcondition: { }
+
       }
 
-    };
-    this._apiService.httpViaPost(endpointc, data).subscribe((res: any) => {
-      this.date_search_source_count = res.count;
+      this.subscriptions[this.subscriptioncount++] = this._apiService.httpViaPost(endpointc, data).subscribe((res: any) => {
+          // console.log('in constructor');
+          // console.log(result);
+          this.date_search_source_count = res.count;
+          //console.warn('blogData c',res);
 
-    }, error => {
-      console.log('Oooops!');
-    });
+      }, error => {
+          console.log('Oooops!');
+      });
 
-    this._apiService.httpViaPost(endpoint, data).subscribe((response: any) => {
-      this.myTable = response.result.res;
-      if (this.myTable.length == 0) {
-        this.contactuslistFlag = true;
-      }
-
-    }, error => {
-      console.log('Oooops!');
-    });
+      this.subscriptions[this.subscriptioncount++] = this._apiService.httpViaPost(endpoint, data).subscribe((res: any) => {
+          this.pendingmodelapplicationarray = res.result.res;
+      }, error => {
+          console.log('Oooops!');
+      });
 
   }
-
 
   ngOnInit() {
+
+  }
+  listenLiblistingChange(val: any) {
+      console.log('listenLiblistingChange', val);
   }
 
+  ngOnDestroy() {
+      // prevent memory leak when component destroyed
+      this.subscriptions.forEach(s => s.unsubscribe());
+  }
 }
